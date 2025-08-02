e4f7c8b12bd9221b9d82ceb99b5416a28eafd39b
RyanStoffel <772612@calbaptist.edu>
2025-08-02 16:27:31 -0700

    Added a lot of config to get swayosd and swaync working.

--- nixos/nixos/hosts/default/home.nix
+++ nixos/nixos/hosts/default/home.nix
@@ -18,5 +18,201 @@
     EDITOR = "nvim";
   };
 
+  # Enable SwayNC (Sway Notification Center)
+  services.swaync = {
+    enable = true;
+    settings = {
+      positionX = "right";
+      positionY = "top";
+      layer = "overlay";
+      control-center-layer = "top";
+      layer-shell = true;
+      cssPriority = "application";
+      control-center-margin-top = 0;
+      control-center-margin-bottom = 0;
+      control-center-margin-right = 0;
+      control-center-margin-left = 0;
+      notification-2fa-action = true;
+      notification-inline-replies = false;
+      notification-icon-size = 64;
+      notification-body-image-height = 100;
+      notification-body-image-width = 200;
+      timeout = 10;
+      timeout-low = 5;
+      timeout-critical = 0;
+      fit-to-screen = true;
+      control-center-width = 500;
+      control-center-height = 600;
+      notification-window-width = 500;
+      keyboard-shortcuts = true;
+      image-visibility = "when-available";
+      transition-time = 200;
+      hide-on-clear = false;
+      hide-on-action = true;
+      script-fail-notify = true;
+      scripts = {
+        example-script = {
+          exec = "echo 'Do something...'";
+          urgency = "Normal";
+        };
+      };
+    };
+    style = ''
+      .notification-row {
+        outline: none;
+      }
+
+      .notification-row:focus,
+      .notification-row:hover {
+        background: @surface0;
+      }
+
+      .notification {
+        border-radius: 12px;
+        margin: 6px 12px;
+        box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 3px 1px rgba(0, 0, 0, 0.7),
+          0 2px 6px 2px rgba(0, 0, 0, 0.2);
+        padding: 0;
+      }
+
+      .notification-content {
+        background: transparent;
+        padding: 6px;
+        border-radius: 12px;
+      }
+
+      .close-button {
+        background: @red;
+        color: @crust;
+        text-shadow: none;
+        padding: 0;
+        border-radius: 100%;
+        margin-top: 10px;
+        margin-right: 16px;
+        box-shadow: none;
+        border: none;
+        min-width: 24px;
+        min-height: 24px;
+      }
+
+      .close-button:hover {
+        box-shadow: none;
+        background: @maroon;
+        transition: all 0.15s ease-in-out;
+        border: none;
+      }
+
+      .notification-default-action,
+      .notification-default-action:hover {
+        padding: 6px;
+        margin: 0;
+        box-shadow: none;
+        background: @base;
+        border: 1px solid @surface0;
+        color: @text;
+        transition: all 0.15s ease-in-out;
+      }
+
+      .notification-default-action:hover {
+        -gtk-icon-effect: none;
+        background: @surface0;
+      }
+
+      .notification-default-action .notification-content {
+        background: transparent;
+        color: @text;
+        text-shadow: none;
+        padding: 6px;
+        border-radius: 12px;
+      }
+
+      .low {
+        background: @surface0;
+        padding: 6px;
+        border-radius: 12px;
+      }
+
+      .normal {
+        background: @surface0;
+        padding: 6px;
+        border-radius: 12px;
+      }
+
+      .critical {
+        background: @red;
+        padding: 6px;
+        border-radius: 12px;
+      }
+
+      .control-center {
+        background: @base;
+        border: 1px solid @surface0;
+        border-radius: 12px;
+      }
+
+      .control-center-list {
+        background: transparent;
+      }
+
+      .control-center-list-placeholder {
+        opacity: 0.5;
+      }
+
+      .floating-notifications {
+        background: transparent;
+      }
+
+      .blank-window {
+        background: alpha(black, 0.25);
+      }
+
+      .widget-title {
+        color: @text;
+        background: @surface0;
+        padding: 5px 10px;
+        border-radius: 12px;
+      }
+
+      .widget-title > button {
+        font-size: 16px;
+        color: @text;
+        text-shadow: none;
+        background: @surface0;
+        border: 1px solid @surface1;
+        border-radius: 12px;
+        box-shadow: none;
+      }
+
+      .widget-title > button:hover {
+        background: @surface1;
+      }
+
+      .widget-dnd {
+        background: @surface0;
+        padding: 5px 10px;
+        border-radius: 12px;
+        color: @text;
+      }
+
+      .widget-dnd > switch {
+        border-radius: 12px;
+        background: @surface1;
+      }
+
+      .widget-dnd > switch:checked {
+        background: @mauve;
+      }
+
+      .widget-dnd > switch slider {
+        background: @text;
+        border-radius: 12px;
+      }
+
+      .widget-dnd > switch:checked slider {
+        background: @crust;
+      }
+    '';
+  };
+
   programs.home-manager.enable = true;
 }
