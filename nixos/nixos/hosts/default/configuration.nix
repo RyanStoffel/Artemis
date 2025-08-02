0847e8903acc401b49b3164fd74d9ea0e382abaf
RyanStoffel <772612@calbaptist.edu>
2025-08-02 16:17:40 -0700

    Fixed error in configuration.nix.

--- nixos/nixos/hosts/default/configuration.nix
+++ nixos/nixos/hosts/default/configuration.nix
@@ -3,34 +3,38 @@
   inputs,
   ...
 }: {
+  # ================================
+  # IMPORTS
+  # ================================
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../modules/config/hyprland-configuration.nix
     inputs.home-manager.nixosModules.default
   ];
 
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
+  # ================================
+  # SYSTEM INFORMATION
+  # ================================
+  system.stateVersion = "25.05";
 
-  # Boot optimizations
+  # ================================
+  # BOOT CONFIGURATION
+  # ================================
   boot = {
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       timeout = 1; # Faster boot menu timeout
     };
+
     plymouth = {
       enable = true;
       theme = "breeze";
     };
-    # Optimize boot process
+
+    # Boot optimizations
     initrd.systemd.enable = true;
-    # Preload AMD GPU modules
-    kernelModules = ["amdgpu"];
-    # Enable early KMS for AMD and disable unused serial ports
+    kernelModules = ["amdgpu"]; # Preload AMD GPU modules
     kernelParams = [
       "amdgpu.dc=1"
       "8250.nr_uarts=0" # Disable serial ports to save 7+ seconds
@@ -39,46 +43,233 @@
     ];
   };
 
-  # Network optimization - Replace dhcpcd with systemd-networkd
+  # ================================
+  # LOCALIZATION & TIME
+  # ================================
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # ================================
+  # NETWORKING
+  # ================================
   networking = {
     hostName = "Artemis";
-    # Disable dhcpcd (the slow culprit)
-    dhcpcd.enable = false;
-    # Use systemd-networkd instead
-    useNetworkd = true;
+    dhcpcd.enable = false; # Disable slow dhcpcd
+    useNetworkd = true; # Use fast systemd-networkd
     wireless.iwd.enable = true;
   };
 
   # Fast systemd-networkd configuration
   systemd.network = {
     enable = true;
-    networks."20-wired" = {
-      matchConfig.Name = "en*";
-      networkConfig = {
-        DHCP = "ipv4";
-        IPv4Forwarding = false;
+    networks = {
+      "20-wired" = {
+        matchConfig.Name = "en*";
+        networkConfig = {
+          DHCP = "ipv4";
+          IPv4Forwarding = false;
+        };
+        dhcpV4Config.RouteMetric = 1024;
       };
-      dhcpV4Config = {
-        RouteMetric = 1024;
-      };"custom/nixos": {
-    "format": "<span size='15000' foreground='#89b4fa'>󱄅</span>",
-    "on-click": "kitty --class=fastfetch-terminal -o allow_remote_control=no -o shell_integration=disabled -o scrollback_lines=0 -o mouse_hide_wait=0 -o cursor_blink_interval=0 -e bash -c 'fastfetch; exec 0</dev/null; while true; do sleep 1; done'"
-  }
-    };
-    networks."25-wireless" = {
-      matchConfig.Name = "wl*";
-      networkConfig = {
-        DHCP = "ipv4";
-        IPv4Forwarding = false;
-      };
-      dhcpV4Config = {
-        RouteMetric = 1025;
+      "25-wireless" = {
+        matchConfig.Name = "wl*";
+        networkConfig = {
+          DHCP = "ipv4";
+          IPv4Forwarding = false;
+        };
+        dhcpV4Config.RouteMetric = 1025;
       };
     };
   };
 
-  # Enable systemd-resolved for DNS
   services.resolved.enable = true;
+
+  # ================================
+  # HARDWARE CONFIGURATION
+  # ================================
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    graphics.enable = true;
+  };
+
+  # ================================
+  # GRAPHICS & DISPLAY
+  # ================================
+  services.xserver.videoDrivers = ["amdgpu"];
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "catppuccin-mocha";
+    package = pkgs.kdePackages.sddm;
+  };
+
+  # ================================
+  # AUDIO
+  # ================================
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+
+  # ================================
+  # USER CONFIGURATION
+  # ================================
+  users.users.rstoffel = {
+    isNormalUser = true;
+    extraGroups = ["networkmanager" "wheel" "audio"];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  # ================================
+  # NIX CONFIGURATION
+  # ================================
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowBroken = true;
+  };
+
+  # ================================
+  # PROGRAMS & APPLICATIONS
+  # ================================
+  programs = {
+    # Browsers
+    firefox.enable = true;
+    chromium = {
+      enable = true;
+      extraOpts = {
+        "default_search_provider.enabled" = true;
+        "default_search_provider.name" = "Kagi";
+        "default_search_provider.search_url" = "https://kagi.com/search?q={searchTerms}";
+        "default_search_provider.keyword" = "kagi.com";
+        "default_search_provider.replace_search_url_in_history" = true;
+        "default_search_provider.id" = 1;
+      };
+    };
+
+    # Shell & Development
+    fish.enable = true;
+    git = {
+      enable = true;
+      config = {
+        user.name = "RyanStoffel";
+        user.email = "772612@calbaptist.edu";
+        init.defaultBranch = "main";
+        pull.rebase = true;
+      };
+    };
+
+    # Gaming
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    gamemode.enable = true;
+  };
+
+  # ================================
+  # SYSTEM PACKAGES
+  # ================================
+  environment.systemPackages = with pkgs; [
+    # === Development Tools ===
+    vim
+    wget
+    kitty
+    starship
+    zoxide
+    eza
+    bat
+    fastfetch
+    stow
+
+    # === Desktop Environment ===
+    rofi-wayland
+    networkmanagerapplet
+    blueberry
+    pavucontrol
+    hyprpaper
+    hyprlock
+    nwg-look
+    nautilus
+    swayosd
+    swaynotificationcenter
+
+    # === Themes & Appearance ===
+    bibata-cursors
+    magnetic-catppuccin-gtk
+    papirus-icon-theme
+    hicolor-icon-theme
+    adwaita-icon-theme
+    (catppuccin-sddm.override {
+      flavor = "mocha";
+      font = "JetBrainsMono Nerd Font";
+      fontSize = "11";
+      background = "${../../assets/wallpapers/wallpaper1.png}";
+      loginBackground = true;
+    })
+
+    # === Gaming ===
+    mangohud
+    protonup
+    prismlauncher
+
+    # === System Utilities ===
+    libnotify
+    pipewire
+    wireplumber
+    chromium
+    impala
+    iwd
+    btop
+  ];
+
+  # ================================
+  # FONTS
+  # ================================
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = ["JetBrainsMono Nerd Font"];
+        sansSerif = ["JetBrainsMono Nerd Font"];
+        monospace = ["JetBrainsMono Nerd Font"];
+      };
+    };
+  };
+
+  # ================================
+  # ENVIRONMENT VARIABLES
+  # ================================
+  environment.variables = {
+    EDITOR = "nvim";
+    XCURSOR_THEME = "Bibata-Modern-Classic";
+    XCURSOR_SIZE = "24";
+    MOZ_ENABLE_WAYLAND = "1";
+    PULSE_RUNTIME_PATH = "/run/user/1000/pulse";
+  };
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATH = "/home/rstoffel/.steam/root/compatibilitytools.d";
+  };
+
+  # ================================
+  # SYSTEM SERVICES & OPTIMIZATION
+  # ================================
+  services = {
+    openssh.enable = true;
+  };
 
   # Journal optimization (reduce the 2.2s journal flush time)
   services.journald.extraConfig = ''
@@ -95,187 +286,13 @@
     DefaultTimeoutStartSec=10s
   '';
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # AMD GPU configuration
-  hardware.graphics = {
-    enable = true;
-  };
-  services.xserver.videoDrivers = ["amdgpu"];
-
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-    theme = "catppuccin-mocha";
-    package = pkgs.kdePackages.sddm;
-  };
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    jack.enable = true;
-    wireplumber.enable = true;
-  };
-
-  # Define a user account. Don't forget to set a password with 'passwd'.
-  users.users.rstoffel = {
-    isNormalUser = true;
-    extraGroups = ["networkmanager" "wheel" "audio"];
-    packages = with pkgs; [
-      tree
-    ];
-    shell = pkgs.fish;
-  };
-
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowBroken = true;
-
-  # Packages & Programs.
-  programs.firefox.enable = true;
-  programs.chromium = {
-    enable = true;
-    extraOpts = {
-      "default_search_provider.enabled" = true;
-      "default_search_provider.name" = "Kagi";
-      "default_search_provider.search_url" = "https://kagi.com/search?q={searchTerms}";
-      "default_search_provider.keyword" = "kagi.com";
-      "default_search_provider.replace_search_url_in_history" = true;
-      "default_search_provider.id" = 1;
-    };
-  };
-  programs.fish.enable = true;
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
-  programs.git = {
-    enable = true;
-    config = {
-      user.name = "RyanStoffel";
-      user.email = "772612@calbaptist.edu";
-      init.defaultBranch = "main";
-      pull.rebase = true;
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    # Development
-    vim
-    wget
-    kitty
-    starship
-    zoxide
-    eza
-    bat
-    fastfetch
-    stow
-    # Applications
-    rofi-wayland
-    mangohud
-    protonup
-    prismlauncher
-    networkmanagerapplet
-    blueberry
-    pavucontrol
-    hyprpaper
-    bibata-cursors
-    libnotify
-    pipewire
-    wireplumber
-    nwg-look
-    magnetic-catppuccin-gtk
-    nautilus
-    hyprlock
-    (catppuccin-sddm.override
-      {
-        flavor = "mocha";
-        font = "JetBrainsMono Nerd Font";
-        fontSize = "11";
-        background = "${../../assets/wallpapers/wallpaper1.png}";
-        loginBackground = true;
-      })
-    chromium
-    impala
-    iwd
-    papirus-icon-theme
-    hicolor-icon-theme
-    adwaita-icon-theme
-    btop
-    swayosd
-    swaynotificationcenter
-  ];
-
-  # Font configuration - make JetBrains Mono the system default
-  fonts = {
-    packages = with pkgs; [
-      nerd-fonts.jetbrains-mono
-    ];
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-        serif = ["JetBrainsMono Nerd Font"];
-        sansSerif = ["JetBrainsMono Nerd Font"];
-        monospace = ["JetBrainsMono Nerd Font"];
-      };
-    };
-  };
-
-  environment.variables = {
-    EDITOR = "nvim";
-    XCURSOR_THEME = "Bibata-Modern-Classic";
-    XCURSOR_SIZE = "24";
-    MOZ_ENABLE_WAYLAND = "1";
-    PULSE_RUNTIME_PATH = "/run/user/1000/pulse";
-  };
-
-  environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATH = "/home/rstoffel/.steam/root/compatibilitytools.d";
-  };
-
+  # ================================
+  # HOME MANAGER
+  # ================================
   home-manager = {
-    # also pass inputs to home-manager modules
     extraSpecialArgs = {inherit inputs;};
     users = {
       "rstoffel" = import ./home.nix;
     };
   };
-
-  # List services that you want to enable:
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
 }
